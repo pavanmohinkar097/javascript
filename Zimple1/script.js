b12854364d623312d207450bbcbb5932db2143f7
pavanmohinkar097 <pavanmohi097@gmail.com>
2023-03-23 14:57:31 +0530

    22-03-2023

--- Zimple1/script.js
+++ Zimple1/script.js
@@ -1,1 +1,70 @@
-console.log(document.body.firstChild)
+// console.log(console)
+// console.error("thisn is error")
+// obj={a:1, b:2, c:3, d:5}
+// console.table(obj)
+// alert = ("Enter the value a!")
+// let a = prompt("Enter value here","56")
+// a=Number.parseInt(a)
+// alert("You entered a of type" +(typeof a))
+// document.write(a)
+
+// alert("Enter the value a!")
+// let a = prompt("Enter value here","56")
+// a=Number.parseInt(a)
+// alert("You entered a of type " +(typeof a))
+// let write=confirm("Do you want to write to the page")
+// if(write){
+//     document.write(a)
+// }
+// else{
+//     document.write("Please allow me to write")
+
+// }
+// document.write(a)
+
+
+
+// Exampl 1 (Practies set 6)
+
+// let age = prompt("Enter your age")
+//     age = Number.parseInt(age)
+//     const canDrive = (age) => {
+//         return age >= 18 ? true : false
+//     }
+//     if (canDrive(age)) {
+//         alert("Yes you can drive")
+//     }
+//     else {
+//         alert("No you cannot drive")
+//     }
+
+// let color= prompt("Enter the page background color")
+// document.body.style.background= color
+
+// let arr=['snake','water','gun'];
+//         alert("you are playing snake water and gun game,")
+//         let round=1;
+//         let userGuesses=0;
+//         let computerGuesses=0;
+//         while(round<=3)
+//         {
+//             let userInput=prompt("enter your guess");
+//             let computerInput=arr[Math.floor(Math.random()*3)];
+//             if((userInput=='snake' && computerInput=='water') || (userInput=='gun' && computerInput=='snake') || (userInput=='water' && computerInput=='gun')){
+//                 userGuesses++;
+//             }
+//             else{
+//                 computerGuesses++;
+//             }
+//             round++;
+//         }
+
+//         if(computerGuesses>userGuesses)
+//         {
+//             alert("Ohh no You lose the computer scores more than you");
+//         }
+//         else{
+//             alert("yeh thats my boy tu hi jeetega pata hi tha mujhe");
+//         }
+
+ console.log(document.body.firstChild)
