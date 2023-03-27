c2e81fb1c27a187459107b31da7257c1beb58277
pavanmohinkar097 <pavanmohi097@gmail.com>
2023-03-27 18:05:03 +0530

    26-03-2023

---
+++ demo1/app.js
@@ -0,0 +1,11 @@
+let ctitle=document.getElementById("firstcardtitle")
+ctitle.style.color="blue"
+
+let ctitles=document.querySelectorAll(".card-title")
+ctitles[0].style.color="blue"
+ctitles[1].style.color="red"
+ctitles[2].style.color="orange"
+document.querySelector(".this").style.color="red"
+document.querySelector(".this").style.background="black"
+console.log(document.getElementsByTagName("a"))
+console.log(document.querySelector(".card").getElementsByTagName("a"))
