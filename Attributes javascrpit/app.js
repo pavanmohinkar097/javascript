9d766ee2d586929e836c8670821db26eb121c4be
pavanmohinkar097 <pavanmohi097@gmail.com>
2023-03-29 14:51:46 +0530

    29/time 2-51

---
+++ Attributes javascrpit/app.js
@@ -0,0 +1,17 @@
+// let first=document.getElementById("first");
+// let a=first.hasAttribute("class")
+// console.log(a)
+// let b=first.hasAttribute("style")
+// console.log(b)
+
+
+
+{/* <div id="first">
+        I am first container
+    </div>
+first.removeAttribute("class") */}
+// console.log(first.dataes)
+let first=document.getElementById('first');
+console.log(first.dataset);
+console.log(first.dataset.game);
+console.log(first.dataset.player);
