27ba38b0135d0a762330b96f6eb9e136a73f24b1
pavanmohinkar097 <pavanmohi097@gmail.com>
2023-03-29 23:55:16 +0530

    29/03/2023

---
+++ className and classlist/app.js
@@ -0,0 +1,13 @@
+first.class="text-black red"
+
+
+
+// first.className=first.class="text-black red"
+// first.classList.remove("red")
+// undefined
+// first.classList.add("red")
+// undefined
+// first.classList.toggle("red")
+// true
+// first.classList.contains("red")
+// true
